265b7e373e81e4b223e186da791f67eb2dbcf182
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-05-16 11:49:45 +0400

    first_update

---
+++ task.py
@@ -0,0 +1,25 @@
+# Salam,hərvaxtınız xeyir.
+# 1) taskınız ilk olaraq bir text faylı yaradıb içərisinə istədiyiniz bir cümlə yazırsınız .
+# 2) daha sonra həmin textin ilk sətrindəki  sözlərin bütün hərflərini böyük hərflərə çeviririk .
+# 3) Ən sonda bu sözləri başqa  bir text faylı yaradıb onun içərisində yazırıq.
+
+import os
+
+text_content = """lorem ipsum, dolor sit amet consectetur 
+adipisicing elit. Illo harum,suscipit esse asperiores 
+praesentium quae ex pariatur, possimus at exercitationem 
+eius dolores voluptatum laudantium nesciunt unde 
+labore dolorem iusto explicabosoluta velit autem quaerat dicta!
+"""
+path_1 = os.path.dirname(__file__) + "/new_txt_1.txt"
+path_2 = os.path.dirname(__file__) + "/new_txt_2.txt"
+
+with open(path_1, encoding="utf-8", mode="w+") as data:
+    data.write(text_content)
+    data.seek(0)
+    ilk_setir = [i.title() for i in data.readlines() ][0]
+
+
+with open(path_2, encoding="utf-8", mode="w") as data:
+    data.write(ilk_setir)
+
